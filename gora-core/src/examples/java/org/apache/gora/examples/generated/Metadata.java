d30b88ac24af61d4ed4fc954588e25b10c0afc3d
Lewis John McGibbney <lewismc@apache.org>
2013-08-18 21:53:34 +0000

    GORA-264 Make generated data beans more java doc friendly
    git-svn-id: https://svn.apache.org/repos/asf/gora/trunk@1515229 13f79535-47bb-0310-9956-ffa450edef68

--- gora-core/src/examples/java/org/apache/gora/examples/generated/Metadata.java
+++ gora-core/src/examples/java/org/apache/gora/examples/generated/Metadata.java
@@ -38,38 +38,104 @@ import org.apache.gora.persistency.impl.PersistentBase;
 import org.apache.gora.persistency.impl.StateManagerImpl;
 import org.apache.gora.persistency.StatefulHashMap;
 import org.apache.gora.persistency.ListGenericArray;
-
 @SuppressWarnings("all")
 public class Metadata extends PersistentBase {
+  
+  /**
+   * Variable holding the data bean schema.
+   */
   public static final Schema _SCHEMA = Schema.parse("{\"type\":\"record\",\"name\":\"Metadata\",\"namespace\":\"org.apache.gora.examples.generated\",\"fields\":[{\"name\":\"version\",\"type\":\"int\"},{\"name\":\"data\",\"type\":{\"type\":\"map\",\"values\":\"string\"}}]}");
+  
+  /**
+   * Enum containing all data bean's fields.
+   */
   public static enum Field {
     VERSION(0,"version"),
     DATA(1,"data"),
     ;
+    
+    /**
+     * Field's index.
+     */
     private int index;
+    
+    /**
+     * Field's name.
+     */
     private String name;
+    
+    /**
+     * Field's constructor
+     * @param index field's index.
+     * @param name field's name.
+     */
     Field(int index, String name) {this.index=index;this.name=name;}
+    
+    /**
+     * Gets field's index.
+     * @return int field's index.
+     */
     public int getIndex() {return index;}
+    
+    /**
+     * Gets field's name.
+     * @return String field's name.
+     */
     public String getName() {return name;}
+    
+    /**
+     * Gets field's attributes to string.
+     * @return String field's attributes to string.
+     */
     public String toString() {return name;}
   };
+    
+    /**
+     * Contains all field's names.
+     */
   public static final String[] _ALL_FIELDS = {"version","data",};
   static {
     PersistentBase.registerFields(Metadata.class, _ALL_FIELDS);
   }
   private int version;
   private Map<Utf8,Utf8> data;
+  
+  /**
+   * Default Constructor
+   */
   public Metadata() {
     this(new StateManagerImpl());
   }
+  
+  /**
+   * Constructor
+   * @param stateManager for the data bean.
+   */
   public Metadata(StateManager stateManager) {
     super(stateManager);
     data = new StatefulHashMap<Utf8,Utf8>();
   }
+  
+  /**
+   * Returns a new instance by using a state manager.
+   * @param stateManager for the data bean.
+   * @return Metadata created.
+   */
   public Metadata newInstance(StateManager stateManager) {
     return new Metadata(stateManager);
   }
+  
+  /**
+   * Returns the schema of the data bean.
+   * @return Schema for the data bean.
+   */
   public Schema getSchema() { return _SCHEMA; }
+  
+  /**
+   * Gets a specific field.
+   * @param field index of a field for the data bean.
+   * @return Object representing a data bean's field.
+   */
   public Object get(int _field) {
     switch (_field) {
     case 0: return version;
@@ -77,6 +143,12 @@ public class Metadata extends PersistentBase {
     default: throw new AvroRuntimeException("Bad index");
     }
   }
+  
+  /**
+   * Puts a value for a specific field.
+   * @param field index of a field for the data bean.
+   * @param value value of a field for the data bean.
+   */
   @SuppressWarnings(value="unchecked")
   public void put(int _field, Object _value) {
     if(isFieldEqual(_field, _value)) return;
@@ -87,23 +159,54 @@ public class Metadata extends PersistentBase {
     default: throw new AvroRuntimeException("Bad index");
     }
   }
+  
+  /**
+   * Gets the Version.
+   * @return int representing Metadata Version.
+   */
   public int getVersion() {
     return (Integer) get(0);
   }
+  
+  /**
+   * Sets the Version.
+   * @param value containing Metadata Version.
+   */
   public void setVersion(int value) {
     put(0, value);
   }
+  
+  /**
+   * Gets Data.
+   * @return Map containing Data value.
+   */
   public Map<Utf8, Utf8> getData() {
     return (Map<Utf8, Utf8>) get(1);
   }
+  
+  /**
+   * Gets the Data's value using a key.
+   * @param key gets a specific Data using a MetadataID.
+   * @return Utf8 containing Data value.
+   */
   public Utf8 getFromData(Utf8 key) {
     if (data == null) { return null; }
     return data.get(key);
   }
+  
+  /**
+   * Adds a Data into a Metadata.
+   * @param Map containing Data value.
+   */
   public void putToData(Utf8 key, Utf8 value) {
     getStateManager().setDirty(this, 1);
     data.put(key, value);
   }
+  
+  /**
+   * Removes Data from a Metadata.
+   * @return key Metadata ID to be removed.
+   */
   public Utf8 removeFromData(Utf8 key) {
     if (data == null) { return null; }
     getStateManager().setDirty(this, 1);
--- gora-core/src/examples/java/org/apache/gora/examples/generated/TokenDatum.java
+++ gora-core/src/examples/java/org/apache/gora/examples/generated/TokenDatum.java
@@ -38,41 +38,113 @@ import org.apache.gora.persistency.impl.PersistentBase;
 import org.apache.gora.persistency.impl.StateManagerImpl;
 import org.apache.gora.persistency.StatefulHashMap;
 import org.apache.gora.persistency.ListGenericArray;
-
 @SuppressWarnings("all")
 public class TokenDatum extends PersistentBase {
+  
+  /**
+   * Variable holding the data bean schema.
+   */
   public static final Schema _SCHEMA = Schema.parse("{\"type\":\"record\",\"name\":\"TokenDatum\",\"namespace\":\"org.apache.gora.examples.generated\",\"fields\":[{\"name\":\"count\",\"type\":\"int\"}]}");
+  
+  /**
+   * Enum containing all data bean's fields.
+   */
   public static enum Field {
     COUNT(0,"count"),
     ;
+    
+    /**
+     * Field's index.
+     */
     private int index;
+    
+    /**
+     * Field's name.
+     */
     private String name;
+    
+    /**
+     * Field's constructor
+     * @param index field's index.
+     * @param name field's name.
+     */
     Field(int index, String name) {this.index=index;this.name=name;}
+    
+    /**
+     * Gets field's index.
+     * @return int field's index.
+     */
     public int getIndex() {return index;}
+    
+    /**
+     * Gets field's name.
+     * @return String field's name.
+     */
     public String getName() {return name;}
+    
+    /**
+     * Gets field's attributes to string.
+     * @return String field's attributes to string.
+     */
     public String toString() {return name;}
   };
+    
+    /**
+     * Contains all field's names.
+     */
   public static final String[] _ALL_FIELDS = {"count",};
   static {
     PersistentBase.registerFields(TokenDatum.class, _ALL_FIELDS);
   }
   private int count;
+  
+  /**
+   * Default Constructor
+   */
   public TokenDatum() {
     this(new StateManagerImpl());
   }
+  
+  /**
+   * Constructor
+   * @param stateManager for the data bean.
+   */
   public TokenDatum(StateManager stateManager) {
     super(stateManager);
   }
+  
+  /**
+   * Returns a new instance by using a state manager.
+   * @param stateManager for the data bean.
+   * @return TokenDatum created.
+   */
   public TokenDatum newInstance(StateManager stateManager) {
     return new TokenDatum(stateManager);
   }
+  
+  /**
+   * Returns the schema of the data bean.
+   * @return Schema for the data bean.
+   */
   public Schema getSchema() { return _SCHEMA; }
+  
+  /**
+   * Gets a specific field.
+   * @param field index of a field for the data bean.
+   * @return Object representing a data bean's field.
+   */
   public Object get(int _field) {
     switch (_field) {
     case 0: return count;
     default: throw new AvroRuntimeException("Bad index");
     }
   }
+  
+  /**
+   * Puts a value for a specific field.
+   * @param field index of a field for the data bean.
+   * @param value value of a field for the data bean.
+   */
   @SuppressWarnings(value="unchecked")
   public void put(int _field, Object _value) {
     if(isFieldEqual(_field, _value)) return;
@@ -82,9 +154,19 @@ public class TokenDatum extends PersistentBase {
     default: throw new AvroRuntimeException("Bad index");
     }
   }
+  
+  /**
+   * Gets the Count.
+   * @return int representing TokenDatum Count.
+   */
   public int getCount() {
     return (Integer) get(0);
   }
+  
+  /**
+   * Sets the Count.
+   * @param value containing TokenDatum Count.
+   */
   public void setCount(int value) {
     put(0, value);
   }
