bcc60b6250ee566f0fac68bcfbeaf93b463268c9
Damien Raude-Morvan <damien@dictanova.com>
2013-12-16 01:10:22 +0100

    Enable new test for MongoMappingBuilder

--- gora-mongodb/src/test/java/org/apache/gora/mongodb/store/TestMongoMappingBuilder.java
+++ gora-mongodb/src/test/java/org/apache/gora/mongodb/store/TestMongoMappingBuilder.java
@@ -15,124 +15,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/*package org.apache.gora.mongodb.store;
+package org.apache.gora.mongodb.store;
 
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 
-import org.apache.gora.mongodb.beans.tests.Host;
-import org.apache.gora.mongodb.beans.tests.Test1;
-import org.apache.gora.mongodb.beans.tests.WebPage;
+import org.apache.gora.examples.generated.WebPage;
 import org.apache.gora.mongodb.store.MongoMapping.DocumentFieldType;
 import org.junit.Test;
 
 public class TestMongoMappingBuilder {
-
-  @Test
-  public void testStraightMapping_Test1() throws IOException {
-    MongoStore<String, Test1> store = new MongoStore<String, Test1>();
-    store.setKeyClass(String.class);
-    store.setPersistentClass(Test1.class);
-    MongoMappingBuilder<String, Test1> builder = new MongoMappingBuilder<String, Test1>(
-        store);
-    builder.fromFile("/org/apache/gora/mongodb/straightmapping-test1.xml");
-    MongoMapping mapping = builder.build();
-
-    // Check collection name
-    assertEquals("test1", mapping.getCollectionName());
-
-    // Check field names
-    assertEquals("mapOfBytes", mapping.getDocumentField("mapOfBytes"));
-    assertEquals("mapOfStrings", mapping.getDocumentField("mapOfStrings"));
-    assertEquals("mapOfInt", mapping.getDocumentField("mapOfInt"));
-    assertEquals("scalarString", mapping.getDocumentField("scalarString"));
-    assertEquals("scalarDate", mapping.getDocumentField("scalarDate"));
-    assertEquals("scalarInt", mapping.getDocumentField("scalarInt"));
-
-    // Check field types
-    assertEquals(DocumentFieldType.DOCUMENT,
-        mapping.getDocumentFieldType("mapOfBytes"));
-    assertEquals(DocumentFieldType.DOCUMENT,
-        mapping.getDocumentFieldType("mapOfStrings"));
-    assertEquals(DocumentFieldType.DOCUMENT,
-        mapping.getDocumentFieldType("mapOfInt"));
-    assertEquals(DocumentFieldType.STRING,
-        mapping.getDocumentFieldType("scalarString"));
-    assertEquals(DocumentFieldType.DATE,
-        mapping.getDocumentFieldType("scalarDate"));
-    assertEquals(DocumentFieldType.INT32,
-        mapping.getDocumentFieldType("scalarInt"));
-  }
-
-  @Test
-  public void testRenameMapping_Test1() throws IOException {
-    MongoStore<String, Test1> store = new MongoStore<String, Test1>();
-    store.setKeyClass(String.class);
-    store.setPersistentClass(Test1.class);
-    MongoMappingBuilder<String, Test1> builder = new MongoMappingBuilder<String, Test1>(
-        store);
-    builder.fromFile("/org/apache/gora/mongodb/renamemapping-test1.xml");
-    MongoMapping mapping = builder.build();
-
-    // Check collection name
-    assertEquals("test1", mapping.getCollectionName());
-    mapping.renameCollection("test1", "newName");
-    assertEquals("newName", mapping.getCollectionName());
-
-    // Check field names
-    assertEquals("maps.m1", mapping.getDocumentField("mapOfBytes"));
-    assertEquals("maps.m2", mapping.getDocumentField("mapOfStrings"));
-    assertEquals("maps.m3", mapping.getDocumentField("mapOfInt"));
-    assertEquals("scalars.f1", mapping.getDocumentField("scalarString"));
-    assertEquals("scalars.f2", mapping.getDocumentField("scalarDate"));
-    assertEquals("scalars.f3", mapping.getDocumentField("scalarInt"));
-
-    // Check field types
-    assertEquals(DocumentFieldType.DOCUMENT,
-        mapping.getDocumentFieldType("maps.m1"));
-    assertEquals(DocumentFieldType.DOCUMENT,
-        mapping.getDocumentFieldType("maps.m2"));
-    assertEquals(DocumentFieldType.DOCUMENT,
-        mapping.getDocumentFieldType("maps.m3"));
-    assertEquals(DocumentFieldType.STRING,
-        mapping.getDocumentFieldType("scalars.f1"));
-    assertEquals(DocumentFieldType.DATE,
-        mapping.getDocumentFieldType("scalars.f2"));
-    assertEquals(DocumentFieldType.INT32,
-        mapping.getDocumentFieldType("scalars.f3"));
-  }
-
-  @Test
-  public void testMultiMapping_Host() throws IOException {
-    MongoStore<String, Host> store = new MongoStore<String, Host>();
-    store.setKeyClass(String.class);
-    store.setPersistentClass(Host.class);
-    MongoMappingBuilder<String, Host> builder = new MongoMappingBuilder<String, Host>(
-        store);
-    builder.fromFile("/org/apache/gora/mongodb/multimapping.xml");
-    MongoMapping mapping = builder.build();
-
-    // Check collection name
-    assertEquals("hosts", mapping.getCollectionName());
-    mapping.renameCollection("hosts", "newNameForHosts");
-    assertEquals("newNameForHosts", mapping.getCollectionName());
-
-    // Check field names
-    assertEquals("metadata", mapping.getDocumentField("metadata"));
-    assertEquals("links.out", mapping.getDocumentField("outlinks"));
-    assertEquals("links.in", mapping.getDocumentField("inlinks"));
-
-    // Check field types
-    assertEquals(DocumentFieldType.DOCUMENT,
-        mapping.getDocumentFieldType("metadata"));
-    assertEquals(DocumentFieldType.DOCUMENT,
-        mapping.getDocumentFieldType("links"));
-    assertEquals(DocumentFieldType.DOCUMENT,
-        mapping.getDocumentFieldType("links.out"));
-    assertEquals(DocumentFieldType.DOCUMENT,
-        mapping.getDocumentFieldType("links.in"));
-  }
 
   @Test
   public void testMultiMapping_Webpage() throws IOException {
@@ -141,7 +34,7 @@ public class TestMongoMappingBuilder {
     store.setPersistentClass(WebPage.class);
     MongoMappingBuilder<String, WebPage> builder = new MongoMappingBuilder<String, WebPage>(
         store);
-    builder.fromFile("/org/apache/gora/mongodb/multimapping.xml");
+    builder.fromFile("/multimapping.xml");
     MongoMapping mapping = builder.build();
 
     // Check collection name
@@ -217,4 +110,3 @@ public class TestMongoMappingBuilder {
         mapping.getDocumentFieldType("metadata"));
   }
 }
-*/
