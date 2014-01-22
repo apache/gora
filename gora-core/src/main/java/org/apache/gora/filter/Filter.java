0dc2f1b20b4f47cbd18cd8c89b5f1e010e91d51f
Lewis John McGibbney <lewismc@apache.org>
2014-01-22 16:29:05 +0000

    GORA-119 implement a filter enabled scan in gora
    git-svn-id: https://svn.apache.org/repos/asf/gora/trunk@1560408 13f79535-47bb-0310-9956-ffa450edef68

---
+++ gora-core/src/main/java/org/apache/gora/filter/Filter.java
@@ -0,0 +1,42 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.gora.filter;
+
+import org.apache.gora.persistency.Persistent;
+import org.apache.hadoop.io.Writable;
+
+/**
+ * Defines filtering (possibly including modification) of rows. By default
+ * all filtering is done client side. (In generic Gora classes). Datastore
+ * implementations can decide if they install remote filters, when possible.
+ *  
+ * @param <K>
+ * @param <T>
+ */
+public interface Filter<K, T extends Persistent> extends Writable{
+  
+  /**
+   * Filter the key and persistent. Modification is possible.
+   * 
+   * @param key
+   * @param persistent
+   * @return <code>true</code> if the row is filtered out (excluded), 
+   * <code>false</code> otherwise.
+   */
+  public boolean filter(K key, T persistent);
+}
