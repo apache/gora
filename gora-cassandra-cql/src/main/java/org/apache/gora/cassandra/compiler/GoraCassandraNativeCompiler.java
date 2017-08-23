ce78ef520c44a075dd24d02f41edd4cccf47bc4b
madhawa-gunasekara <madhawag@wso2.com>
2017-08-24 00:33:27 +0530

    Fix code review comments

--- gora-cassandra-cql/src/main/java/org/apache/gora/cassandra/compiler/GoraCassandraNativeCompiler.java
+++ gora-cassandra-cql/src/main/java/org/apache/gora/cassandra/compiler/GoraCassandraNativeCompiler.java
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.gora.cassandra.compiler;
 
 import org.apache.commons.io.FilenameUtils;
--- gora-cassandra-cql/src/main/java/org/apache/gora/cassandra/serializers/AvroCassandraUtils.java
+++ gora-cassandra-cql/src/main/java/org/apache/gora/cassandra/serializers/AvroCassandraUtils.java
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.gora.cassandra.serializers;
 
 import org.apache.avro.Schema;
