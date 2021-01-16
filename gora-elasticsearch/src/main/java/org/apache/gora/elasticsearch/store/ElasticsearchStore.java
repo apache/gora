474a3946ebfde25732fe16d6546aa479fc6509a0
Maria Podorvanova <podorvanova.maria@gmail.com>
2021-01-16 23:49:18 +1100

    Implement some basic Input-Output operations for schema management
    - Implemented delete, get and put methods
    - Implemented newInstance and getUnionSchema utility methods
    - Implemented basic serialization/deserialization for primitive AVRO types

--- gora-elasticsearch/src/main/java/org/apache/gora/elasticsearch/store/ElasticsearchStore.java
+++ gora-elasticsearch/src/main/java/org/apache/gora/elasticsearch/store/ElasticsearchStore.java
@@ -16,14 +16,18 @@
  */
 package org.apache.gora.elasticsearch.store;
 
+import org.apache.avro.Schema;
 import org.apache.gora.elasticsearch.mapping.ElasticsearchMapping;
 import org.apache.gora.elasticsearch.mapping.ElasticsearchMappingBuilder;
+import org.apache.gora.elasticsearch.mapping.Field;
 import org.apache.gora.elasticsearch.utils.ElasticsearchParameters;
+import org.apache.gora.persistency.Persistent;
 import org.apache.gora.persistency.impl.PersistentBase;
 import org.apache.gora.query.PartitionQuery;
 import org.apache.gora.query.Query;
 import org.apache.gora.query.Result;
 import org.apache.gora.store.impl.DataStoreBase;
+import org.apache.gora.util.AvroUtils;
 import org.apache.gora.util.GoraException;
 import org.apache.http.Header;
 import org.apache.http.HttpHost;
@@ -33,9 +37,14 @@ import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.reactor.IOReactorConfig;
 import org.apache.http.message.BasicHeader;
+import org.elasticsearch.action.DocWriteResponse;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.admin.indices.flush.FlushRequest;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
@@ -48,11 +57,10 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 /**
  * Implementation of a Apache Elasticsearch data store to be used by Apache Gora.
@@ -224,17 +232,69 @@ public class ElasticsearchStore<K, T extends PersistentBase> extends DataStoreBa
 
     @Override
     public T get(K key, String[] fields) throws GoraException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        String[] requestedFields = getFieldsToQuery(fields);
+        List<String> documentFields = new ArrayList<>();
+        for (String requestedField : requestedFields) {
+            documentFields.add(elasticsearchMapping.getFields().get(requestedField).getName());
+        }
+        try {
+            // Prepare the Elasticsearch request
+            GetRequest getRequest = new GetRequest(elasticsearchMapping.getIndexName(), (String) key);
+            GetResponse getResponse = client.get(getRequest, RequestOptions.DEFAULT);
+            Map<String, Object> sourceMap = getResponse.getSourceAsMap();
+
+            // Map of field's name and its value from the Document
+            Map<String, Object> fieldsAndValues = new HashMap<>();
+            for (String field : documentFields) {
+                fieldsAndValues.put(field, sourceMap.get(field));
+            }
+
+            // Build the corresponding persistent
+            return newInstance(fieldsAndValues, requestedFields);
+        } catch (IOException ex) {
+            throw new GoraException(ex);
+        }
     }
 
     @Override
     public void put(K key, T obj) throws GoraException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (obj.isDirty()) {
+            Schema schemaObj = obj.getSchema();
+            List<Schema.Field> fields = schemaObj.getFields();
+            Map<String, Object> jsonMap = new HashMap<>();
+            for (Schema.Field field : fields) {
+                Field mappedField = elasticsearchMapping.getFields().get(field.name());
+                if (mappedField != null) {
+                    Object fieldValue = obj.get(field.pos());
+                    if (fieldValue != null) {
+                        Schema fieldSchema = field.schema();
+                        Object serializedObj = serializeFieldValue(fieldSchema, fieldValue);
+                        jsonMap.put(mappedField.getName(), serializedObj);
+                    }
+                }
+            }
+            // Prepare the Elasticsearch request
+            IndexRequest request = new IndexRequest(elasticsearchMapping.getIndexName()).id((String) key).source(jsonMap);
+            try {
+                client.index(request, RequestOptions.DEFAULT);
+            } catch (IOException ex) {
+                throw new GoraException(ex);
+            }
+        } else {
+            LOG.info("Ignored putting object {} in the store as it is neither "
+                    + "new, neither dirty.", new Object[]{obj});
+        }
     }
 
     @Override
     public boolean delete(K key) throws GoraException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        DeleteRequest request = new DeleteRequest(elasticsearchMapping.getIndexName(), (String) key);
+        try {
+            DeleteResponse deleteResponse = client.delete(request, RequestOptions.DEFAULT);
+            return deleteResponse.getResult() != DocWriteResponse.Result.NOT_FOUND;
+        } catch (IOException ex) {
+            throw new GoraException(ex);
+        }
     }
 
     @Override
@@ -274,5 +334,157 @@ public class ElasticsearchStore<K, T extends PersistentBase> extends DataStoreBa
         } catch (IOException ex) {
             LOG.error(ex.getMessage(), ex);
         }
+    }
+
+    public T newInstance(Map<String, Object> fieldsAndValues, String[] requestedFields) throws IOException {
+        // Create new empty persistent bean instance
+        T persistent = newPersistent();
+
+        requestedFields = getFieldsToQuery(requestedFields);
+        // Populate each field
+        for (String objField : requestedFields) {
+            Schema.Field field = fieldMap.get(objField);
+            Schema fieldSchema = field.schema();
+            String docFieldName = elasticsearchMapping.getFields().get(objField).getName();
+            Object fieldValue = fieldsAndValues.get(docFieldName);
+
+            Object result = deserializeFieldValue(field, fieldSchema, fieldValue, persistent);
+            persistent.put(field.pos(), result);
+        }
+        persistent.clearDirty();
+        return persistent;
+    }
+
+    private Object deserializeFieldValue(Schema.Field field, Schema fieldSchema,
+                                         Object elasticsearchValue, T persistent) {
+        Object fieldValue = null;
+        switch (fieldSchema.getType()) {
+            case ARRAY:
+            case BOOLEAN:
+            case BYTES:
+            case FIXED:
+            case MAP:
+            case RECORD:
+                throw new UnsupportedOperationException();
+            case NULL:
+                fieldValue = null;
+                break;
+            case UNION:
+                Schema.Type type0 = fieldSchema.getTypes().get(0).getType();
+                Schema.Type type1 = fieldSchema.getTypes().get(1).getType();
+                if (fieldSchema.getTypes().size() == 2 &&
+                        (type0.equals(Schema.Type.NULL) || type1.equals(Schema.Type.NULL)) &&
+                        !type0.equals(type1)) {
+                    int schemaPos = getUnionSchema(elasticsearchValue, fieldSchema);
+                    Schema unionSchema = fieldSchema.getTypes().get(schemaPos);
+                    fieldValue = deserializeFieldValue(field, unionSchema, elasticsearchValue, persistent);
+                } else if (fieldSchema.getTypes().size() == 3) {
+                    Schema.Type type2 = fieldSchema.getTypes().get(2).getType();
+                    if ((type0.equals(Schema.Type.NULL) || type1.equals(Schema.Type.NULL) || type2.equals(Schema.Type.NULL)) &&
+                            (type0.equals(Schema.Type.STRING) || type1.equals(Schema.Type.STRING) || type2.equals(Schema.Type.STRING))) {
+                        //TODO: Deserialize recursively or do another request
+                    }
+                    throw new UnsupportedOperationException();
+                }
+                break;
+            case DOUBLE:
+                fieldValue = Double.parseDouble(elasticsearchValue.toString());
+                break;
+            case ENUM:
+                fieldValue = AvroUtils.getEnumValue(fieldSchema, elasticsearchValue.toString());
+                break;
+            case FLOAT:
+                fieldValue = Float.parseFloat(elasticsearchValue.toString());
+                break;
+            case INT:
+                fieldValue = Integer.parseInt(elasticsearchValue.toString());
+                break;
+            case LONG:
+                fieldValue = Long.parseLong(elasticsearchValue.toString());
+                break;
+            case STRING:
+                fieldValue = elasticsearchValue.toString();
+                break;
+            default:
+                fieldValue = elasticsearchValue;
+        }
+        return fieldValue;
+    }
+
+    private Object serializeFieldValue(Schema fieldSchema, Object fieldValue) {
+        Object output = fieldValue;
+        switch (fieldSchema.getType()) {
+            case ARRAY:
+            case BOOLEAN:
+            case BYTES:
+            case ENUM:
+            case FIXED:
+            case MAP:
+            case RECORD:
+            case UNION:
+                throw new UnsupportedOperationException();
+            case DOUBLE:
+            case FLOAT:
+            case INT:
+            case LONG:
+            case STRING:
+                output = fieldValue.toString();
+                break;
+            case NULL:
+                output = null;
+                break;
+        }
+        return output;
+    }
+
+    private int getUnionSchema(Object instanceValue, Schema unionSchema) {
+        int unionSchemaPos = 0;
+        for (Schema currentSchema : unionSchema.getTypes()) {
+            Schema.Type schemaType = currentSchema.getType();
+            if (instanceValue instanceof CharSequence && schemaType.equals(Schema.Type.STRING)) {
+                return unionSchemaPos;
+            }
+            if (instanceValue instanceof ByteBuffer && schemaType.equals(Schema.Type.BYTES)) {
+                return unionSchemaPos;
+            }
+            if (instanceValue instanceof byte[] && schemaType.equals(Schema.Type.BYTES)) {
+                return unionSchemaPos;
+            }
+            if (instanceValue instanceof Integer && schemaType.equals(Schema.Type.INT)) {
+                return unionSchemaPos;
+            }
+            if (instanceValue instanceof Long && schemaType.equals(Schema.Type.LONG)) {
+                return unionSchemaPos;
+            }
+            if (instanceValue instanceof Double && schemaType.equals(Schema.Type.DOUBLE)) {
+                return unionSchemaPos;
+            }
+            if (instanceValue instanceof Float && schemaType.equals(Schema.Type.FLOAT)) {
+                return unionSchemaPos;
+            }
+            if (instanceValue instanceof Boolean && schemaType.equals(Schema.Type.BOOLEAN)) {
+                return unionSchemaPos;
+            }
+            if (instanceValue instanceof Map && schemaType.equals(Schema.Type.MAP)) {
+                return unionSchemaPos;
+            }
+            if (instanceValue instanceof List && schemaType.equals(Schema.Type.ARRAY)) {
+                return unionSchemaPos;
+            }
+            if (instanceValue instanceof Persistent && schemaType.equals(Schema.Type.RECORD)) {
+                return unionSchemaPos;
+            }
+            if (instanceValue instanceof byte[] && schemaType.equals(Schema.Type.MAP)) {
+                return unionSchemaPos;
+            }
+            if (instanceValue instanceof byte[] && schemaType.equals(Schema.Type.RECORD)) {
+                return unionSchemaPos;
+            }
+            if (instanceValue instanceof byte[] && schemaType.equals(Schema.Type.ARRAY)) {
+                return unionSchemaPos;
+            }
+            unionSchemaPos++;
+        }
+        return 0;
     }
 }
